8d4a28ed49faee282e6cf4e1434a18378338043b
Afnan Dzaharudin <vucirrha@gmail.com>
2022-11-05 19:48:28 +0000

    documentation

--- entry/main.cpp
+++ entry/main.cpp
@@ -1,3 +1,10 @@
+/**
+ * @file main.cpp
+ *
+ * Expected to be used for quick testing. A new entryfile should be made
+ * if we expect to be doing something very often.
+ */
+
 #include <iostream>
 
 int main()
