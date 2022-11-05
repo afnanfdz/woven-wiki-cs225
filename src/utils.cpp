72ad8015bef723a2933c7d63ef87d13a6c6dbae1
Afnan Dzaharudin <vucirrha@gmail.com>
2022-11-05 19:34:36 +0000

    implemented file-reading functions, mainly for filtering

---
+++ src/utils.cpp
@@ -0,0 +1,93 @@
+#include "utils.h"
+
+#include <fstream>
+#include <vector>
+#include <string>
+#include <exception>
+
+#include <sstream>
+#include <iostream>
+
+vector<pair<unsigned int, unsigned int>> importEdges(string file_dir)
+{
+    vector<pair<unsigned int, unsigned int>> result;
+
+    ifstream file;
+    string line;
+
+    file.open(file_dir);
+
+    if (file.is_open())
+    {
+        unsigned int node_1;
+        unsigned int node_2;
+
+        while (file >> node_1 >> node_2)
+        {
+            result.push_back({node_1, node_2});
+        }
+    }
+
+    return result;
+}
+
+void shrinkEdgeData(string file_dir, string file_out_dir, int max_idx)
+{
+    ofstream output;
+    ifstream file;
+
+    file.open(file_dir);
+    output.open(file_out_dir);
+
+    if (file.is_open())
+    {
+        string node_1;
+        string node_2;
+
+        while (file >> node_1 >> node_2)
+        {
+            if (stoi(node_1) <= max_idx && stoi(node_2) <= max_idx)
+            {
+                output << node_1 << " " << node_2 << "\n";
+            }
+        }
+    }
+    else
+    {
+        string err = "Could not find input directory " + file_dir + "; assuming this is used on the Topcats data please manually install it first (see data/README.md)";
+        throw std::invalid_argument(err);
+    }
+
+    file.close();
+    output.close();
+}
+
+void shrinkNames(string file_dir, string file_out_dir, int max_idx)
+{
+    ofstream output;
+    ifstream file;
+
+    file.open(file_dir);
+    output.open(file_out_dir);
+
+    if (file.is_open())
+    {
+        string line; // eg. 29 Missouri Route 147
+
+        while (getline(file, line))
+        {
+            if (stoi(line.substr(0, line.find(' '))) > max_idx)
+                break;
+            else
+                output << line << "\n";
+        }
+    }
+    else
+    {
+        string err = "Could not find input directory " + file_dir + "; assuming this is used on the Topcats data please manually install it first (see data/README.md)";
+        throw std::invalid_argument(err);
+    }
+
+    file.close();
+    output.close();
+}
---
+++ src/utils.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include <fstream>
+#include <vector>
+#include <utility>
+#include <string>
+
+using std::ifstream;
+using std::ofstream;
+using std::pair;
+using std::string;
+using std::vector;
+
+/***
+ * @param file_dir: directory to the .txt file
+ * @return a vector of directed edge relationships of numbered nodes
+ **/
+vector<pair<unsigned int, unsigned int>> importEdges(string file_dir);
+
+/***
+ * Takes edge data (int to int) and filters it to only contain
+ * relations for nodes equal to or below the input max_idx.
+ *
+ * Does NOT assume the nodes are indexed in order, nor are without gaps.
+ *
+ * @param file_dir: Directory to the .txt file
+ * @param file_out_dir: Directory to output the new file
+ * @param new_size: The largest node index to allow
+ *
+ * @return Writes the file to `file_out_dir`
+ */
+void shrinkEdgeData(string file_dir, string file_out_dir, int max_idx);
+
+/***
+ * Takes node name data (int to string) and filters it to only contain
+ * the names of nodes equal to or below the input max_idx
+ *
+ * Assumes the nodes are listed in increasing index number.
+ *
+ * @param file_dir: Directory to the .txt file
+ * @param file_out_dir: Directory to output the new file
+ * @param new_size: The largest node index to allow
+ *
+ * @return Writes the file to `file_out_dir`
+ */
+void shrinkNames(string file_dir, string file_out_dir, int max_idx);
