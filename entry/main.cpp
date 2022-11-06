c7a543a7abb108694430cc17afaa46f819825222
Afnan Dzaharudin <vucirrha@gmail.com>
2022-11-06 20:07:26 +0000

    cleared out main again

--- entry/main.cpp
+++ entry/main.cpp
@@ -5,22 +5,6 @@
  * if we expect to be doing something very often.
  */
 
-#include <iostream>
-
-#include <fstream>
-#include <string>
-#include <vector>
-#include <sstream>
-
-#include "utils.h"
-
-using std::ifstream;
-using std::string;
-using std::stringstream;
-using std::vector;
-
 int main()
 {
-    shrinkCategories("../data/wiki-topcats-categories.txt", "../data/gendata/cats.txt", 50);
-    return 0;
 }
