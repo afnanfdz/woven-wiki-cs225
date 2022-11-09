ffa7358b3ddac543f3964c7cb8d149fa695d0898
Afnan Dzaharudin <vucirrha@gmail.com>
2022-11-09 23:05:56 +0000

    getting started on graph implementation

---
+++ src/graph.cpp
@@ -0,0 +1,35 @@
+#include <graph.h>
+
+template <class T>
+Graph<T>::Graph()
+{
+}
+
+template <class T>
+Graph<T>::Graph(Graph &other)
+{
+}
+
+template <class T>
+Graph<T> &Graph<T>::operator=(Graph &other)
+{
+}
+
+template <class T>
+Graph<T>::~Graph()
+{
+}
+
+template <class T>
+void Graph<T>::copy(Graph &other)
+{
+}
+
+template <class T>
+void Graph<T>::clear()
+{
+    // for (Node *n : graph_)
+    // {
+    //     delete n;
+    // }
+}
---
+++ src/graph.h
@@ -0,0 +1,64 @@
+#pragma once
+
+#include <unordered_map>
+#include <list>
+
+/**
+ * Graph class. Stores nodes in an unordered map to optimize for access time.
+ * Stores neighbors in a list to optimize insertion time.
+ *
+ */
+template <class T>
+class Graph
+{
+private:
+    /**
+     * Simple container for data
+     * Expected to be used on the heap
+     */
+    struct Node
+    {
+        T val_;
+    };
+
+public:
+    /**
+     * Default constructor. Does nothing.
+     */
+    Graph();
+
+    /**
+     * Copy constructor. Creates a deep copy of the input Graph
+     * @param other Reference to the Graph object
+     */
+    Graph(Graph &other);
+
+    /**
+     * Copy assignment. Creates a deep copy of the RHS Graph
+     * @param other Reference to the Graph object
+     *
+     * @return Reference to the LHS Graph
+     */
+    Graph &operator=(Graph &other);
+
+    /**
+     * Destructor.
+     *
+     * @returns Deletes all node pointers stored.
+     */
+    ~Graph();
+
+private:
+    /**
+     * Helper for the copy and assignment constructor.
+     */
+    void copy(Graph &other);
+
+    /**
+     * Helper for the copy constructors and destructor
+     */
+    void clear();
+
+private:
+    std::unordered_map<Node *, std::list<Node *>> graph_;
+};
