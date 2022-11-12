c2d8a4b6ba4363764ab2819257c97921badd639d
Afnan Dzaharudin <vucirrha@gmail.com>
2022-11-12 21:20:11 +0000

    usable!

---
+++ src/graph.h
@@ -0,0 +1,73 @@
+#pragma once
+
+#include <unordered_map>
+#include <list>
+#include <string>
+#include <iostream>
+
+using std::list;
+using std::string;
+using std::unordered_map;
+
+/**
+ * Graph class. Inspired by the CS225 lib but meant to be more lightweight.
+ * Unweighted. Directed.
+ *
+ */
+template <class V>
+class Graph
+{
+private:
+public:
+    /**
+     * Default constructor. Does nothing.
+     */
+    Graph();
+
+    /**
+     * Takes data from a .txt file of lines of whitespace-seperated pairs.
+     *
+     */
+    void importData(string file_dir);
+
+    /**
+     * @returns Whether the edge exists in the graph.
+     */
+    bool hasEdge(V from, V to) const;
+
+    /**
+     * Adds an edge to the graph.
+     * Will not add duplicate edges.
+     */
+    void addEdge(V from, V to);
+
+    /**
+     * @returns Whether the vertex exists in the graph.
+     */
+    bool hasVertex(V val) const;
+
+    /**
+     * Adds a vertex to the graph.
+     * Will replace old vertex data if existing.
+     */
+    void addVertex(V val);
+
+    /**
+     * Gets all adjacent vertices to the parameter vertex.
+     * @param source - vertex to get neighbors from
+     * @return a vector of vertices
+     */
+    list<V> &getAdjacent(V source) const;
+
+    /**
+     * Prints each node and all its children
+     */
+    void print();
+
+private:
+    unordered_map<V, list<V>>
+        graph_;
+};
+
+// This line is why I cried and shidded and farded
+#include "graph.hpp"
---
+++ src/graph.hpp
@@ -0,0 +1,103 @@
+#include <fstream>
+#include <utility>
+
+using std::ifstream;
+
+template <class V>
+Graph<V>::Graph()
+{
+}
+
+template <class V>
+void Graph<V>::importData(string file_dir)
+{
+    ifstream file;
+    string line;
+
+    file.open(file_dir);
+
+    if (file.is_open())
+    {
+        V from;
+        V to;
+
+        while (file >> from >> to)
+        {
+            if (!hasVertex(from))
+            {
+                addVertex(from);
+            }
+
+            addEdge(from, to);
+        }
+    }
+}
+
+template <class V>
+bool Graph<V>::hasEdge(V from, V to) const
+{
+    bool result = false;
+
+    if (graph_.find(from) != graph_.end())
+    {
+        for (V edge_to : graph_.at(from))
+        {
+            if (edge_to == to)
+            {
+                result = true;
+                break;
+            }
+        }
+    }
+
+    return result;
+}
+
+template <class V>
+void Graph<V>::addEdge(V from, V to)
+{
+    if (!hasEdge(from, to))
+    {
+        graph_.at(from).push_back(to);
+    }
+}
+
+template <class V>
+bool Graph<V>::hasVertex(V val) const
+{
+    return graph_.find(val) != graph_.end();
+}
+
+template <class V>
+void Graph<V>::addVertex(V val)
+{
+    if (hasVertex(val))
+    {
+        graph_.at(val).clear();
+    }
+    else
+    {
+        graph_.insert({val, list<V>()});
+    }
+}
+
+template <class V>
+list<V> &Graph<V>::getAdjacent(V source) const
+{
+    return graph_.at(source);
+}
+
+template <class V>
+void Graph<V>::print()
+{
+    for (std::pair<const V, list<V>> &p : graph_)
+    {
+        std::cout << p.first << " -> [ ";
+
+        for (V dest : p.second)
+        {
+            std::cout << dest << " ";
+        }
+        std::cout << "]" << std::endl;
+    }
+}
