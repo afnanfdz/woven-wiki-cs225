65932f538f95c7def4c71a13a9652232cb732ee4
Afnan Dzaharudin <vucirrha@gmail.com>
2022-11-14 18:46:15 +0000

    documentation

--- src/wikisearchBFS.cpp
+++ src/wikisearchBFS.cpp
@@ -6,24 +6,34 @@ using std::queue;
 
 vector<int> WikiSearch::shortestPathBFS(int begin, int goal) const
 {
+    // Initialize the pathtrace to memorize visited articles
     Pathtrace<int> pathtrace(begin, goal);
-    queue<int> frontier;
 
+    // Initialize the frontier to perform a breadth-first search
+    queue<int> frontier;
     frontier.push(begin);
 
+    // Iterate while there's vertices to visit,
+    // and while the goal is not visited
     while (!frontier.empty() && !pathtrace.goalIsFound())
     {
+        // Pop and store the front value
         int front = frontier.front();
         frontier.pop();
 
+        // Iterate over all articles it has a link to
         for (const int &adj : graph_.getAdjacent(front))
         {
+            // If a linked article has not yet been discovered,
+            // mark as visited and add it to the frontier
             if (!pathtrace.visited(adj))
             {
                 pathtrace.insert({adj, front});
                 frontier.push(adj);
             }
         }
+
+        // Loop until the while conditions are broken.
     }
 
     return pathtrace.getShortestPath(); // Returns empty if not found
