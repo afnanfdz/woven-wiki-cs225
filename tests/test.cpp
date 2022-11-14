68927b50ec99188ac3b055cbbf52da1187611fd2
Afnan Dzaharudin <vucirrha@gmail.com>
2022-11-14 19:50:49 +0000

    development

--- tests/test.cpp
+++ tests/test.cpp
@@ -24,6 +24,11 @@ using std::vector;
 
 /** UTILITIES FOR TESTS */
 
+/**
+ * @author Vuc
+ * Imports the handwritten true shortest path for a given graph.
+ * Assumes the handwritten true shottest path was... correctly written.
+ */
 vector<int> readSolutionPath(string file_dir)
 {
     ifstream file;
@@ -32,6 +37,21 @@ vector<int> readSolutionPath(string file_dir)
     stringstream ss;
 
     return vector<int>();
+}
+
+/**
+ * @author Vuc
+ * Iterates over two vectors of 'article' / vertex paths
+ * Asserts they are of the same size. Asserts the contents are identical.
+ */
+void comparePaths(const vector<int> &path_1, const vector<int> &path_2)
+{
+    REQUIRE(path_1.size() == path_2.size());
+
+    for (size_t idx = 0; i < path_1.size(); i++)
+    {
+        REQUIRE(path_1.at(idx) == path_2.at(idx));
+    }
 }
 
 /** END OF TEST UTILITIES */
@@ -43,6 +63,11 @@ vector<int> readSolutionPath(string file_dir)
 /** TEST CASES */
 
 TEST_CASE("Example Test", "[tag]")
+{
+    REQUIRE(true);
+}
+
+TEST_CASE("Correct Shortest Paths", "[correctShortestPaths]")
 {
     REQUIRE(true);
 }
