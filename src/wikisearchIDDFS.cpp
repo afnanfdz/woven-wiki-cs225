04160b5e63eb180d85c3913a8214a1ff6635683e
Afnan Dzaharudin <vucirrha@gmail.com>
2022-11-23 17:15:08 +0000

    completion & documentation

--- src/wikisearchIDDFS.cpp
+++ src/wikisearchIDDFS.cpp
@@ -2,19 +2,55 @@
 
 #include <stack>
 #include <limits>
+#include <algorithm>
 
 using std::stack;
 
-const int WikiSearch::END_OF_PATH = std::numeric_limits<int>::max();
-
-vector<int> WikiSearch::limitedDFS(int start, int goal, int limit) const
+vector<int> WikiSearch::limitedDFS(int source, int goal, int limit) const
 {
-    return vector<int>(); // Returns empty if not found
+    // Base case 1: We reached the goal
+    if (source == goal)
+    {
+        return vector<int>({goal}); // Found!
+    }
+
+    // Base case 2: End of search
+    if (limit <= 0)
+    {
+        return vector<int>(); // Empty
+    }
+
+    vector<int> result;
+
+    for (const int &adj : getAdjacent(source))
+    {
+        result = limitedDFS(adj, goal, limit - 1);
+
+        // If not empty, it's been found; collapse it up!
+        if (!result.empty())
+        {
+            result.push_back(source);
+            break;
+        }
+    }
+
+    return result;
 }
 
-vector<int> WikiSearch::shortestPathIDDFS(int begin, int goal) const
+vector<int> WikiSearch::shortestPathIDDFS(int start, int goal) const
 {
-    int max_depth = getDisjointSet().size(begin);
+    int max_depth = getDisjointSet().size(start);
+    vector<int> result;
 
-    return vector<int>(); // Returns empty if not found
+    for (int depth = 0; depth <= max_depth; depth++)
+    {
+        result = limitedDFS(start, goal, depth);
+        if (!result.empty())
+        {
+            std::reverse(result.begin(), result.end());
+            break;
+        }
+    }
+
+    return result; // Returns empty if not found
 }
