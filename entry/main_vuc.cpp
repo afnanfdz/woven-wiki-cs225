d564566db73088cccd3cc52909ebd2850d837662
Afnan Dzaharudin <vucirrha@gmail.com>
2022-11-23 07:17:09 +0000

    some testing

--- entry/main_vuc.cpp
+++ entry/main_vuc.cpp
@@ -2,7 +2,28 @@
  * @file main_vuc.cpp
  */
 
+#include "wikisearch.h"
+#include <iostream>
+
 int main()
 {
-    return 0;
+    WikiSearch ws;
+
+    ws.importData("../data/wiki-nodes.txt");
+
+    // ws.getDisjointSet().print();
+    std::cout << "Number of disjoint sets: " << ws.getDisjointSet().getNumSets() << std::endl;
+
+    int count = 0;
+    for (int i : ws.getNodes())
+    {
+        ++count;
+
+        std::cout << "Max depth of " << i << ": " << ws.getDisjointSet().size(i) << std::endl;
+
+        if (count > 100)
+        {
+            break;
+        }
+    }
 }
